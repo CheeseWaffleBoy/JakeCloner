212d73204cb71c8b2246ce262cebdf2c3d407543
CheeseWaffleBoy <Aiden.Baas@gmail.com>
2026-02-27 20:55:17 -0500

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import { useState } from "react";
 import './App.css';
+import myImage from "./IMG_4539.jpg";
 
-function App() {
+function Button() {
    const [count, setCount] = useState(0);
 
   function handleClick() {
     setCount(count + 1);
+    const audio = new Audio("../Click.mp3");
+  audio.play();
   }
+
 
   return (
      <button
       style={{
-        width: "100px",
-        height: "100px",
-        backgroundImage: 'url("IMG_4539.jpg")',
+        width: "504px",
+        height: "671px",
         backgroundSize: "cover",
+        marginLeft: "0px",
+        marginTop: "0px"
       }}
-      onClick={handleClick}
+      onClick={handleClick}  >
+
+      <img src={myImage}
+       alt="My Image"
+       style={{
+        width: "500px",
+        height: "auto",
+        marginLeft: "-6px",
+        marginTop: "0px" 
+        }}/>
       
-    >
-      Cloned {count} times
     </button>
+    
+
   );
 }
+function App() {
+   return (
+    <div>
+     <h1 style={{
+       position: "absolute",
+       top: "50%",
+       left: "50%",
+       color: "black"
+     }}>
+       Hello
+     </h1>
+    </div>
+   );
+}
 
-export default App;
+export default Button;
